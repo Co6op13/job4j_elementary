227740726b53a6aff04b4d8b582d016aea99a51f
Co6op13 <Co6op13@yandex.ru>
2022-07-28 15:13:17 +0500

    test

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir Dunyushkin");
+        System.out.println("18.12.1987");
+
+    }
+}
