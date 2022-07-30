b312960fec0d3ef96dc798ad1d91486100dab1c9
Co6op13 <Co6op13@yandex.ru>
2022-07-30 11:17:35 +0500

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -13,5 +13,10 @@ public class Fit {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
+
+        height = 170;
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man 187 is " + woman);
+
     }
 }
