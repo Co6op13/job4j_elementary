a90b1dd37d170c0b6851ccbc428d3f99e38869c2
Co6op13 <Co6op13@yandex.ru>
2022-07-31 11:17:05 +0500

    4.5. Отладка программы в IDEA [#159464 #327246]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,18 +2,12 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = third;
-        if (first == second || second == third || third == first) {
-            System.out.print("Присутствуют равные числа это");
-            System.out.print(first == second ? "first & second " : first == third ? " first & third" : "second == third");
-            int equal = first == second ? first : first == third ? first : third;
-            result = first >= equal ? first : second >= equal ? second : third >= equal ? third : equal;
-        }
-        if (first > second && first > third) {
-            result = first;
-        }
-        if (second > first && second > third) {
+        int result = first;
+        if (second >= first  && second >= third) {
             result = second;
+        }
+        if (third >= first && third >= second) {
+            result = third;
         }
         return result;
     }
