f9ed1253bb1ee27ec23bb9f5d9c543d2abafdd55
Co6op13 <Co6op13@yandex.ru>
2022-07-31 11:15:03 +0500

    4.5. Отладка программы в IDEA [#159464 #327246] неверно

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,12 +2,18 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = first;
-        if (second >= first  && second >= third) {
-            result = second;
+        int result = third;
+        if (first == second || second == third || third == first) {
+            System.out.print("Присутствуют равные числа это");
+            System.out.print(first == second ? "first & second " : first == third ? " first & third" : "second == third");
+            int equal = first == second ? first : first == third ? first : third;
+            result = first >= equal ? first : second >= equal ? second : third >= equal ? third : equal;
         }
-        if (third >= first && third >= second) {
-            result = third;
+        if (first > second && first > third) {
+            result = first;
+        }
+        if (second > first && second > third) {
+            result = second;
         }
         return result;
     }
@@ -15,7 +21,7 @@ public class ThreeMax {
     public static int maxTest(int first, int second, int third) {
         return maxOf2numbers(maxOf2numbers(first, second), third);
     }
-
+    
     private static int maxOf2numbers(int x, int y) {
         return (x + y + Math.abs(x - y)) / 2;
     }
