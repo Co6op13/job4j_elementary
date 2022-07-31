10f663e54ebc50d896db0ee96ece13993f0898cb
Co6op13 <Co6op13@yandex.ru>
2022-07-31 11:08:04 +0500

    4.3.5. Множественное логическое выражение И [#9461 #327196]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,22 +2,21 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = third;
-        if (first == second || second == third || third == first) {
-            System.out.print("Присутствуют равные числа это");
-            System.out.print(first == second ? "first & second " : first == third ? " first & third" : "second == third");
-            int equal = first == second ? first : first == third ? first : third;
-
-            result = first >= equal ? first : second >= equal ? second : third >= equal ? third : equal;
-
-        }
-        if (first > second && first > third) {
-            result = first;
-        }
-        if (second > first && second > third) {
+        int result = first;
+        if (second >= first  && second >= third) {
             result = second;
         }
-
+        if (third >= first && third >= second) {
+            result = third;
+        }
         return result;
+    }
+
+    public static int maxTest(int first, int second, int third) {
+        return maxOf2numbers(maxOf2numbers(first, second), third);
+    }
+
+    private static int maxOf2numbers(int x, int y) {
+        return (x + y + Math.abs(x - y)) / 2;
     }
 }
