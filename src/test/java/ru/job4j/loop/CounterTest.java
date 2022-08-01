0f9aef377fb96905bae693d4315358a4ad08c424
Co6op13 <Co6op13@yandex.ru>
2022-08-01 10:32:21 +0500

    test

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromOneToSixThen12() {
+        int start = 1;
+        int finish = 6;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 12;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromOneToOneHundredThen2550() {
+        int start = 1;
+        int finish = 100;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 2550;
+        assertThat(result).isEqualTo(expected);
+    }
+}
