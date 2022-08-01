d0e19b3261de1eb10c3169075404ad35aae39dc0
Co6op13 <Co6op13@yandex.ru>
2022-08-01 23:23:56 +0500

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double credit = amount;
-        while (credit > 0) {
-            credit += credit / 100 * percent;
-            credit -= salary;
+        while (amount > 0) {
+            amount += amount / 100 * percent;
+            amount -= salary;
             year++;
         }
         return year;
