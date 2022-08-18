511b516f879c480fe0a8e13c28d41a05bda99a31
Co6op13 <Co6op13@yandex.ru>
2022-08-18 23:04:03 +0500

    Рефакторинг - Расстояние между точками

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -18,8 +18,5 @@ public class Point {
         Point point1 = new Point(1, 1);
         Point point2 = new Point(-1, -1);
         System.out.println("result (1, 1) to (-1, -1) " + point1.distance(point2));
-//        System.out.println("result (1, 1) to (-1, -1) " + Point.distance(1, 1, -1, -1));
-//        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 0, 2, 0));
-//        System.out.println("result (10, 1) to (20, 111) " + Point.distance(10, 1, 20, 111));
     }
 }
