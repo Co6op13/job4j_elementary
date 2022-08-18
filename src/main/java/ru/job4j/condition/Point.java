fca54fdbd40277e11e391296df0994ea8dbd2e1b
Co6op13 <Co6op13@yandex.ru>
2022-08-18 22:58:28 +0500

    Рефакторинг - Расстояние между точками

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,13 +2,24 @@ package ru.job4j.condition;
 
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return  Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int horizontal, int vertical) {
+        this.x = horizontal;
+        this.y = vertical;
+    }
+
+    public double distance(Point another) {
+        return  Math.sqrt(Math.pow(this.x - another.x, 2) + Math.pow((this.y - another.y), 2));
     }
 
     public static void main(String[] args) {
-        System.out.println("result (1, 1) to (-1, -1) " + Point.distance(1, 1, -1, -1));
-        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 0, 2, 0));
-        System.out.println("result (10, 1) to (20, 111) " + Point.distance(10, 1, 20, 111));
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(-1, -1);
+        System.out.println("result (1, 1) to (-1, -1) " + point1.distance(point2));
+//        System.out.println("result (1, 1) to (-1, -1) " + Point.distance(1, 1, -1, -1));
+//        System.out.println("result (0, 0) to (2, 0) " + Point.distance(0, 0, 2, 0));
+//        System.out.println("result (10, 1) to (20, 111) " + Point.distance(10, 1, 20, 111));
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,66 +7,54 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(2, 0);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when11to22then1dot41() {
         double expected = 1.41;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(2, 2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenM1M1to22then4do24() {
         double expected = 4.24;
-        int x1 = -1;
-        int y1 = -1;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(-1, -1);
+        Point point2 = new Point(2, 2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenM11toM22then1do41() {
         double expected = 1.41;
-        int x1 = -1;
-        int y1 = 1;
-        int x2 = -2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(-1, 1);
+        Point point2 = new Point(-2, 2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when1M1to2M2then1do41() {
         double expected = 1.41;
-        int x1 = 1;
-        int y1 = -1;
-        int x2 = 2;
-        int y2 = -2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(1, -1);
+        Point point2 = new Point(2, -2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when1001to2002then1do41() {
         double expected = 100;
-        int x1 = 100;
-        int y1 = 1;
-        int x2 = 200;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(100, 1);
+        Point point2 = new Point(200, 2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
